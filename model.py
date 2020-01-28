08223d53ba403f78f58da32a9ed206559c6b29ed
rfelts <rfelts@adobe.com>
2020-01-28 12:00:17 -0800

    Adding code to prevent xss

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from playhouse.db_url import connect
 
 db = connect(os.environ.get('DATABASE_URL', 'sqlite:///my_database.db'))
 
+
 class Message(Model):
     content = CharField(max_length=1024, unique=True)
 
